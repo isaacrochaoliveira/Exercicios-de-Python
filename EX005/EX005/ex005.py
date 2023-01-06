6f4501f6de0e245fa510231e60cb9b28525e5ee2
Isaac Victor Rocha Oliveira <isaacvroliveira@gmail.com>
2023-01-05 22:58:29 -0300

    EX005

---
+++ EX005/EX005/ex005.py
@@ -0,0 +1,4 @@
+n = int(input('Informe um número: '))
+# Para calcularmos o antecessor basta fazermos o mesmo número receber -1
+print(f'Analisando o valor {n}. O seu antecessor é {n - 1}, e seu sucessor é {n + 1}')
+# EX: 6 - 1 = 5|| 6 + 1 = 7
